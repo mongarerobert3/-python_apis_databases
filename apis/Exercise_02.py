38b81b1507838b0a212a7028892d8f003b2d2f16
courte_labs <courte_labs@kali.courte_labs>
2022-09-05 00:21:52 +0300

    Exercise 02

--- apis/Exercise_02.py
+++ apis/Exercise_02.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+import email
 import requests
+from pprint import pprint
 '''
 Building on the previous example, create a list of all of the emails of the users and print
 the list to the console.
@@ -7,5 +9,11 @@ the list to the console.
 '''
 base_url = "http://demo.codingnomads.co:8080/tasks_api/users"
 
-body = response.text
-response = requests.get(base_url, json=body)
+response = requests.get(base_url)
+users = response.json()
+
+email_list = []
+
+for i in range(len(users["data"])):
+    email_list.append(users["data"][i]["email"])
+pprint(email_list)
