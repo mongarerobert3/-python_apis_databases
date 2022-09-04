f8ca7fdd04016d7930112cf6bcb6faf13901c25a
courte_labs <courte_labs@kali.courte_labs>
2022-09-05 00:40:23 +0300

    Exercise 03

--- apis/Exercise_03.py
+++ apis/Exercise_03.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+import requests
+from pprint import pprint
 '''
 Write the necessary code to make a POST request to:
 
@@ -8,3 +11,17 @@ and create a user with your information.
 Make a GET request to confirm that your user information has been saved.
 
 '''
+base_url = "http://demo.codingnomads.co:8080/tasks_api/users"
+
+myData = {
+    "first name" : "Robert",
+    "last name" : "Mongare",
+    "email" : "mongarerobert3@courtelabs.ke",
+}
+
+response = requests.post(base_url, json=myData)
+pprint(f"Response Status Code : {response.status_code}")
+
+
+response = requests.get(base_url)
+pprint(f"Response Content : {response.content}")
