d6861c311cff0391e76f4063c9a51002e56bd59b
courte_labs <courte_labs@kali.courte_labs>
2022-09-08 16:09:15 +0300

    Exercise 05 - delete request

--- apis/Exercise_05.py
+++ apis/Exercise_05.py
@@ -1,6 +1,15 @@
+#!/usr/bin/python
+import requests
 '''
 Write a program that makes a DELETE request to remove the user your create in a previous example.
 
 Again, make a GET request to confirm that information has been deleted.
 
 '''
+
+base_url = "http://demo.codingnomads.co:8080/tasks_api/users"
+
+response = requests.delete(base_url + "/55")
+
+#print the status code
+print(f"Response code: {response.status_code}")
