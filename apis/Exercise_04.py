9e85db0fc1f7cc2d93432024b8242b3ac5df8627
courte_labs <courte_labs@kali.courte_labs>
2022-09-08 16:03:44 +0300

    Exercise 04

--- apis/Exercise_04.py
+++ apis/Exercise_04.py
@@ -1,6 +1,25 @@
+#!/usr/bin/env python
+from pprint import pprint
+import requests
+from pprint import pprint
 '''
 Write a program that makes a PUT request to update your user information to a new first_name, last_name and email.
 
 Again make a GET request to confirm that your information has been updated.
 
 '''
+
+base_url = "http://demo.codingnomads.co:8080/tasks_api/users"
+
+myData = {
+    "id" : 55,
+    "first name" : "Brian",
+    "last name" : "Brown",
+    "email" : "BB@courtelabs.ke",
+}
+
+response = requests.put(base_url, json=myData)
+pprint(f"Response Status Code : {response.status_code}")
+
+response = requests.get(base_url)
+pprint(f"Response Content : {response.content}")
