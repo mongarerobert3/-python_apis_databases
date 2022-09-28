bdbeb5219a45af52a1712d8e1eb82ddb130de0f0
mongarerobert3 <mongarerobert3@gmail.com>
2022-09-28 16:44:22 +0300

    Update statement

--- databases/Exercise_03.py
+++ databases/Exercise_03.py
@@ -4,3 +4,15 @@ Update all films in the film table to a rental_duration value of 10,
 if the length of the movie is more than 150.
 
 '''
+from pprint import pprint
+import sqlalchemy
+
+engine = sqlalchemy.create_engine(f'mysql+pymysql://root:{password}@localhost/sakila')
+connection = engine.connect()
+metadata = sqlalchemy.MetaData()
+
+film = sqlalchemy.Table('film', metadata, autoload=True, autoload_with=engine)
+
+query = sqlalchemy.update(film).values(rental_duration=10).where(film.columns.length > 150)
+
+result= connection.execute(query)
