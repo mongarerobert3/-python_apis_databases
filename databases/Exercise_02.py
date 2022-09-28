c35dc32650556720fcb272a86e6ba91c3e960351
mongarerobert3 <mongarerobert3@gmail.com>
2022-09-28 16:43:11 +0300

    Exe 02

--- databases/Exercise_02.py
+++ databases/Exercise_02.py
@@ -15,3 +15,32 @@ Consider each of the tasks below as a separate database query. Using SQLAlchemy,
 - Using one of the statements above, add a ORDER BY statement of your choice
 
 '''
+from pprint import pprint
+import sqlalchemy
+
+engine = sqlalchemy.create_engine(f'mysql+pymysql://root:{password}@localhost/sakila')
+connection = engine.connect()
+metadata = sqlalchemy.MetaData()
+
+actor = sqlalchemy.Table('actor', metadata, autoload=True, autoload_with=engine)
+film = sqlalchemy.Table('film', metadata, autoload=True, autoload_with=engine)
+film_actor = sqlalchemy.Table('film_actor', metadata, autoload=True, autoload_with=engine)
+
+join_statement = actor.join(film_actor, film_actor.columns.actor_id == \
+    actor.columns.actor_id).join(film, film.columns.film_id == film_actor.columns.film_id)
+
+query = sqlalchemy.select(
+    [
+        #film.columns.film_id,
+        #film.columns.title,
+        #actor.columns.first_name,
+        #actor.columns.last_name
+        sqlalchemy.func.count(actor.columns.last_name),
+        actor.columns.last_name
+    ]
+    ).select_from(join_statement).group_by(actor.columns.last_name)
+
+result_proxy = connection.execute(query)
+result_set = result_proxy.fetchall()
+
+pprint(result_set)
